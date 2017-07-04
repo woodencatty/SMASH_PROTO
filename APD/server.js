b5bbc3a2228247072ba3d65d50323f655717c4a3
Ahn Jung-euk <woodencatty@gmail.com>
2017-07-04 11:04:06 +0900

    asd

--- APD/server.js
+++ APD/server.js
@@ -4,39 +4,4 @@ const gpio = require('wiring-pi');
 const express = require('express');
 const app = express();
 
-
-app.listen(60001, () => {
-    console.log("device enable");
-});
-
-app.get('/', (req, res) => {
-});
-
-app.get('/audio', (req, res) => {
-  sensor.getAdcAudio();
-});
-
-
-app.get('/env', (req, res) => {
- sensor.getAdcEnv();
-});
-
-app.get('/temp', (req, res) => {
-  res.send(123);
-
-});
-
-app.get('/humi', (req, res) => {
- sensor.getHumi();
-
-});
-
-app.get('/dist', (req, res) => {
- sensor.getDist();
-
-});
-
-app.get('/light', (req, res) => {
- sensor.getAdcLight();
-
-});
+sensor.getTemp();

80e392659eaff4ff72ee983d0679c7d738c12047
Ahn Jung-euk <woodencatty@gmail.com>
2017-07-04 15:05:50 +0900

    inquire Test

--- IDD/network.js
+++ IDD/network.js
@@ -1,32 +1,15 @@
-//IDD
-var btSerial = new (require('bluetooth-serial-port')).BluetoothSerialPort();
+const BluetoothSerialPort = require('../lib/bluetooth-serial-port');
 
-btSerial.inquire();
-console.log('finding..');
-btSerial.on('found', function(address, name) {
-    console.log(address);
-    console.log(name);
-	btSerial.findSerialPortChannel(address, function(channel) {
-        console.log('channel finding..');
+const rfcomm = new BluetoothSerialPort.BluetoothSerialPort();
 
-		btSerial.connect(address, channel, function() {
-			console.log('connected');
-
-			btSerial.write(new Buffer('my data', 'utf-8'), function(err, bytesWritten) {
-				if (err) console.log(err);
-			});
-
-			btSerial.on('data', function(buffer) {
-				console.log(buffer.toString('utf-8'));
-			});
-		}, function () {
-			console.log('cannot connect');
-		});
-
-		// close the connection when you're ready
-		btSerial.close();
-	}, function() {
-		console.log('found nothing');
-	});
+rfcomm.on('found', function (address, name) {
+	console.log('found device:', name, 'with address:', address);
 });
 
+rfcomm.on('finished', function () {
+  console.log('inquiry finished');
+});
+
+console.log('start inquiry');
+rfcomm.inquire();
+console.log('should be displayed before the end of inquiry');

3d461eb91dc75cf9b8322a26741ef6acdbe20f8b
Ahn Jung-euk <woodencatty@gmail.com>
2017-07-04 10:55:06 +0900

    asd

--- APD/sensor.js
+++ APD/sensor.js
@@ -22,8 +22,9 @@ var adcLight = 2;                  //ADC Channel 1
     gpio.pinMode(ultraECHO, gpio.INPUT);
 
 module.exports.getTemp = function() {
+    console.log('get');
     temp.read(22, DHT22, function (err, temperature, humidity) {
-        if (!err) {
+        if (!err) {}
             console.log("temp :" + temperature.toFixed(1) + "C");
             output_temp = temperature.toFixed(1);
         }

9bd3980695f9d1b0c8f3113c7aa73fb80d5e7293
Ahn Jung-euk <woodencatty@gmail.com>
2017-07-04 11:07:16 +0900

    asd

--- APD/server.js
+++ APD/server.js
@@ -1,11 +1,5 @@
 var sensor = require('./sensor.js')
 
-const gpio = require('wiring-pi');
-const express = require('express');
-const app = express();
-
-
-
 setTimeout(function(){
   sensor.getTemp();
 }, 2000);

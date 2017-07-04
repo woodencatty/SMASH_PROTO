dd49b97304cf205860f74f96ce73886736f0b090
Ahn Jung-euk <woodencatty@gmail.com>
2017-07-04 15:30:12 +0900

    ..

--- APD/network.js
+++ APD/network.js
@@ -2,7 +2,7 @@
 var async = require('async');
 var noble = require('noble');
 
-var peripheralIdOrAddress = process.argv[2].toLowerCase();
+var peripheralIdOrAddress = process.argv[2];
 
 noble.on('stateChange', function(state) {
   if (state === 'poweredOn') {

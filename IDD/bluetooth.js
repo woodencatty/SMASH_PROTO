c4dfce2bf1d82263cc94f5b2bf9aa2b29912edbc
Ahn Jung-euk <woodencatty@gmail.com>
2017-07-04 20:25:27 +0900

    sample?

--- IDD/bluetooth.js
+++ IDD/bluetooth.js
@@ -1,1 +1,54 @@
 //IDD
+
+var bleno = require('bleno');
+var util = require('util');
+
+var ButtonCharacteristic = function () {
+  ButtonCharacteristic.super_.call(this, {
+    uuid: 'ffe1',
+    properties: ['notify'],
+    descriptors: [
+      new bleno.Descriptor({
+        uuid: '2901',
+        value: 'Button'
+      })
+    ]
+  });
+};
+util.inherits(ButtonCharacteristic, bleno.Characteristic);
+
+ButtonCharacteristic.prototype.onSubscribe = function (maxValueSize, updateValueCallback) {
+  console.log('ButtonCharacteristic subscribe');
+
+  var value1 = 0;
+    console.log('button ' + value1);
+    var data = new Buffer(1);
+    data[0] = value1;
+    updateValueCallback(data);
+
+};
+
+var buttonService = new bleno.PrimaryService({
+  uuid: 'ffe0',
+  characteristics: [
+    new ButtonCharacteristic()
+  ]
+});
+
+bleno.on('stateChange', function (state) {
+  console.log('on -> stateChange: ' + state);
+
+  if (state === 'poweredOn') {
+    bleno.startAdvertising('Button', [buttonService.uuid]);
+  } else {
+    bleno.stopAdvertising();
+  }
+});
+
+bleno.on('advertisingStart', function (error) {
+  console.log('on -> advertisingStart: ' + (error ? 'error ' + error : 'success'));
+
+  if (!error) {
+    bleno.setServices([buttonService]);
+  }
+});

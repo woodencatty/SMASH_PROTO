74e8245c7fab543148a9bec23aaf2e0a548dded5
Ahn Jung-euk <woodencatty@gmail.com>
2017-07-05 10:29:58 +0900

    Testing Value Send

--- IDD/bluetooth.js
+++ IDD/bluetooth.js
@@ -1,119 +1,41 @@
+var bleno = require('bleno');
 var util = require('util');
 
-var bleno = require('bleno');
+var Characteristic = bleno.Characteristic;
+var Descriptor = bleno.Descriptor;
+var PrimaryService = bleno.PrimaryService;
 
+var temperatureSensorId;
 
-var BlenoPrimaryService = bleno.PrimaryService;
-var BlenoCharacteristic = bleno.Characteristic;
-var BlenoDescriptor = bleno.Descriptor;
-
-console.log('bleno');
-
-var StaticReadOnlyCharacteristic = function() {
-  StaticReadOnlyCharacteristic.super_.call(this, {
-    uuid: 'fffffffffffffffffffffffffffffff1',
-    properties: ['read'],
-    value: new Buffer('value'),
+var IDDCharacteristic = function () {
+  IDDCharacteristic.super_.call(this, {
+    uuid: 'bbb1',
+    properties: ['read', 'notify'],
     descriptors: [
-      new BlenoDescriptor({
+      new Descriptor({
         uuid: '2901',
-        value: 'user description'
-      })
-    ]
+        value: 'Temperature'
+      })]
   });
 };
-util.inherits(StaticReadOnlyCharacteristic, BlenoCharacteristic);
+util.inherits(IDDCharacteristic, Characteristic);
 
-var DynamicReadOnlyCharacteristic = function() {
-  DynamicReadOnlyCharacteristic.super_.call(this, {
-    uuid: 'fffffffffffffffffffffffffffffff2',
-    properties: ['read']
-  });
+IDDCharacteristic.prototype.onSubscribe = function (maxValueSize, updateValueCallback) {
+  console.log('IDDCharacteristic subscribe');
+
+  this.changeInterval = setInterval(function () {
+      result = 29.1;
+          var data = new Buffer(4);
+          data.writeFloatLE(result, 0);
+
+          console.log('IDDCharacteristic update value: ' + result);
+          updateValueCallback(data);
+  
+  }.bind(this), 2000);
 };
 
-util.inherits(DynamicReadOnlyCharacteristic, BlenoCharacteristic);
-
-DynamicReadOnlyCharacteristic.prototype.onReadRequest = function(offset, callback) {
-  var result = this.RESULT_SUCCESS;
-  var data = new Buffer('dynamic value');
-
-  if (offset > data.length) {
-    result = this.RESULT_INVALID_OFFSET;
-    data = null;
-  } else {
-    data = data.slice(offset);
-  }
-
-  callback(result, data);
-};
-
-var LongDynamicReadOnlyCharacteristic = function() {
-  LongDynamicReadOnlyCharacteristic.super_.call(this, {
-    uuid: 'fffffffffffffffffffffffffffffff3',
-    properties: ['read']
-  });
-};
-
-util.inherits(LongDynamicReadOnlyCharacteristic, BlenoCharacteristic);
-
-LongDynamicReadOnlyCharacteristic.prototype.onReadRequest = function(offset, callback) {
-  var result = this.RESULT_SUCCESS;
-  var data = new Buffer(512);
-
-  for (var i = 0; i < data.length; i++) {
-    data[i] = i % 256;
-  }
-
-  if (offset > data.length) {
-    result = this.RESULT_INVALID_OFFSET;
-    data = null;
-  } else {
-    data = data.slice(offset);
-  }
-
-  callback(result, data);
-};
-
-var WriteOnlyCharacteristic = function() {
-  WriteOnlyCharacteristic.super_.call(this, {
-    uuid: 'fffffffffffffffffffffffffffffff4',
-    properties: ['write', 'writeWithoutResponse']
-  });
-};
-
-util.inherits(WriteOnlyCharacteristic, BlenoCharacteristic);
-
-WriteOnlyCharacteristic.prototype.onWriteRequest = function(data, offset, withoutResponse, callback) {
-  console.log('WriteOnlyCharacteristic write request: ' + data.toString('hex') + ' ' + offset + ' ' + withoutResponse);
-
-  callback(this.RESULT_SUCCESS);
-};
-
-var NotifyOnlyCharacteristic = function() {
-  NotifyOnlyCharacteristic.super_.call(this, {
-    uuid: 'fffffffffffffffffffffffffffffff5',
-    properties: ['notify']
-  });
-};
-
-util.inherits(NotifyOnlyCharacteristic, BlenoCharacteristic);
-
-NotifyOnlyCharacteristic.prototype.onSubscribe = function(maxValueSize, updateValueCallback) {
-  console.log('NotifyOnlyCharacteristic subscribe');
-
-  this.counter = 0;
-  this.changeInterval = setInterval(function() {
-    var data = new Buffer(4);
-    data.writeUInt32LE(this.counter, 0);
-
-    console.log('NotifyOnlyCharacteristic update value: ' + this.counter);
-    updateValueCallback(data);
-    this.counter++;
-  }.bind(this), 5000);
-};
-
-NotifyOnlyCharacteristic.prototype.onUnsubscribe = function() {
-  console.log('NotifyOnlyCharacteristic unsubscribe');
+IDDCharacteristic.prototype.onUnsubscribe = function () {
+  console.log('IDDCharacteristic unsubscribe');
 
   if (this.changeInterval) {
     clearInterval(this.changeInterval);
@@ -121,106 +43,36 @@ NotifyOnlyCharacteristic.prototype.onUnsubscribe = function() {
   }
 };
 
-NotifyOnlyCharacteristic.prototype.onNotify = function() {
-  console.log('NotifyOnlyCharacteristic on notify');
-};
-
-var IndicateOnlyCharacteristic = function() {
-  IndicateOnlyCharacteristic.super_.call(this, {
-    uuid: 'fffffffffffffffffffffffffffffff6',
-    properties: ['indicate']
-  });
-};
-
-util.inherits(IndicateOnlyCharacteristic, BlenoCharacteristic);
-
-IndicateOnlyCharacteristic.prototype.onSubscribe = function(maxValueSize, updateValueCallback) {
-  console.log('IndicateOnlyCharacteristic subscribe');
-
-  this.counter = 0;
-  this.changeInterval = setInterval(function() {
+IDDCharacteristic.prototype.onReadRequest = function (offset, callback) {
+  sensor.temperature(temperatureSensorId, function (err, result) {
+    console.log('Sensor ' + temperatureSensorId + ' :', result);
     var data = new Buffer(4);
-    data.writeUInt32LE(this.counter, 0);
-
-    console.log('IndicateOnlyCharacteristic update value: ' + this.counter);
-    updateValueCallback(data);
-    this.counter++;
-  }.bind(this), 1000);
-};
-
-IndicateOnlyCharacteristic.prototype.onUnsubscribe = function() {
-  console.log('IndicateOnlyCharacteristic unsubscribe');
-
-  if (this.changeInterval) {
-    clearInterval(this.changeInterval);
-    this.changeInterval = null;
-  }
-};
-
-IndicateOnlyCharacteristic.prototype.onIndicate = function() {
-  console.log('IndicateOnlyCharacteristic on indicate');
-};
-
-function SampleService() {
-  SampleService.super_.call(this, {
-    uuid: 'fffffffffffffffffffffffffffffff0',
-    characteristics: [
-      new StaticReadOnlyCharacteristic(),
-      new DynamicReadOnlyCharacteristic(),
-      new LongDynamicReadOnlyCharacteristic(),
-      new WriteOnlyCharacteristic(),
-      new NotifyOnlyCharacteristic(),
-      new IndicateOnlyCharacteristic()
-    ]
+    data.writeFloatLE(result, 0);
+    callback(Characteristic.RESULT_SUCCESS, data);
   });
-}
+};
 
-util.inherits(SampleService, BlenoPrimaryService);
+var thermometerService = new PrimaryService({
+  uuid: 'bbb0',
+  characteristics: [
+    new IDDCharacteristic()
+  ]
+});
 
-bleno.on('stateChange', function(state) {
-  console.log('on -> stateChange: ' + state + ', address = ' + bleno.address);
+bleno.on('stateChange', function (state) {
+  console.log('on -> stateChange: ' + state);
 
   if (state === 'poweredOn') {
-    bleno.startAdvertising('test', ['fffffffffffffffffffffffffffffff0']);
+    bleno.startAdvertising('Thermometer', [thermometerService.uuid]);
   } else {
     bleno.stopAdvertising();
   }
 });
 
-// Linux only events /////////////////
-bleno.on('accept', function(clientAddress) {
-  console.log('on -> accept, client: ' + clientAddress);
-
-  bleno.updateRssi();
-});
-
-bleno.on('disconnect', function(clientAddress) {
-  console.log('on -> disconnect, client: ' + clientAddress);
-});
-
-bleno.on('rssiUpdate', function(rssi) {
-  console.log('on -> rssiUpdate: ' + rssi);
-});
-//////////////////////////////////////
-
-bleno.on('mtuChange', function(mtu) {
-  console.log('on -> mtuChange: ' + mtu);
-});
-
-bleno.on('advertisingStart', function(error) {
+bleno.on('advertisingStart', function (error) {
   console.log('on -> advertisingStart: ' + (error ? 'error ' + error : 'success'));
 
   if (!error) {
-    bleno.setServices([
-      new SampleService()
-    ]);
+    bleno.setServices([thermometerService]);
   }
-});
-
-bleno.on('advertisingStop', function() {
-  console.log('on -> advertisingStop');
-});
-
-bleno.on('servicesSet', function(error) {
-  console.log('on -> servicesSet: ' + (error ? 'error ' + error : 'success'));
 });

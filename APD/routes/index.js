0808062e469971cfee7a5b06040b109a1df616d2
Ahn Jung-euk <woodencatty@gmail.com>
2017-07-10 11:37:13 +0900

    temp-remove sensor

--- APD/routes/index.js
+++ APD/routes/index.js
@@ -10,7 +10,7 @@ var humidity = 0;
 var audio = 0;
 var enviorment = 0;
 var light = 0;
-
+/*
   this.SensorInterval = setInterval(function () {
 
     DistCallback = function (DistValue) {
@@ -45,13 +45,13 @@ var light = 0;
     sensor.getAdcLight(LightCallback);
 
   }.bind(this), 500);
-
+*/
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log("device enable");
     res.render('index', { title: 'Express' });
   
-  if(distance < 50){
+  if(distance > 50){
     res.redirect('/identify');
   }
 });

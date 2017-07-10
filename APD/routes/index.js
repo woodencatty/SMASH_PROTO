7cb0980f34e22e0596777b3abac609c5ad7f0f3c
Ahn Jung-euk <woodencatty@gmail.com>
2017-07-10 14:50:28 +0900

    code clear

--- APD/routes/index.js
+++ APD/routes/index.js
@@ -11,57 +11,50 @@ var audio = 0;
 var enviorment = 0;
 var light = 0;
 
-  this.SensorInterval = setInterval(function () {
+this.SensorInterval = setInterval(function () {
 
-    DistCallback = function (DistValue) {
-      distance = DistValue;
-    }
+  DistCallback = function (DistValue) {
+    distance = DistValue;
+  }
 
-    TempCallback = function (TempValue) {
-      temperature = TempValue;
-    }
+  TempCallback = function (TempValue) {
+    temperature = TempValue;
+  }
 
-    HumiCallback = function (HumiValue) {
-      humidity = HumiValue;
-    }
+  HumiCallback = function (HumiValue) {
+    humidity = HumiValue;
+  }
 
-    AudCallback = function (AudioValue) {
-      audio = AudioValue;
-    }
+  AudCallback = function (AudioValue) {
+    audio = AudioValue;
+  }
 
-    EnvCallback = function (EnvValue) {
-      enviorment = EnvValue;
-    }
+  EnvCallback = function (EnvValue) {
+    enviorment = EnvValue;
+  }
 
-    LightCallback = function (LightValue) {
-      light = LightValue;
-    }
-    
-   sensor.getDist(DistCallback); 
-sensor.getTemp(TempCallback);
-sensor.getHumi(HumiCallback);
-    sensor.getAdcAudio(AudCallback);
-    sensor.getAdcEnv(EnvCallback);
-    sensor.getAdcLight(LightCallback);
+  LightCallback = function (LightValue) {
+    light = LightValue;
+  }
 
-console.log(distance);
-console.log(temperature);
-console.log(humidity);
-console.log(audio);
-console.log(enviorment);
-console.log(light);
+  sensor.getDist(DistCallback);
+  sensor.getTemp(TempCallback);
+  sensor.getHumi(HumiCallback); 
+  sensor.getAdcAudio(AudCallback);
+  sensor.getAdcEnv(EnvCallback);
+  sensor.getAdcLight(LightCallback);
 
-  }.bind(this), 500);
-  console.log("device enable");
+}.bind(this), 500);
+console.log("device enable");
 /* GET home page. */
-router.get('/main', function(req, res, next) {
+router.get('/main', function (req, res, next) {
   console.log("device enable");
- 
-    res.render('index', { title: '대기화면' });
+
+  res.render('index', { title: '대기화면' });
 });
 
 
-router.get('/identify', function(req, res, next) {
+router.get('/identify', function (req, res, next) {
   res.render('identify', { title: '인식화면' });
 
 });

63a4ac08633b8d5c9638bc13adfda532e3a209b3
Ahn Jung-euk <woodencatty@gmail.com>
2017-07-10 11:27:34 +0900

    replace code

--- APD/routes/index.js
+++ APD/routes/index.js
@@ -1,9 +1,60 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const sensor = require('./sensor.js')
+const bluetooth = require('./bluetooth.js')
+
+var distance = 0;
+var temperature = 0;
+var humidity = 0;
+var audio = 0;
+var enviorment = 0;
+var light = 0;
+
+  this.SensorInterval = setInterval(function () {
+
+    DistCallback = function (DistValue) {
+      distance = DistValue;
+    }
+
+    TempCallback = function (TempValue) {
+      temperature = TempValue;
+    }
+
+    HumiCallback = function (HumiValue) {
+      humidity = HumiValue;
+    }
+
+    AudCallback = function (AudioValue) {
+      audio = AudioValue;
+    }
+
+    EnvCallback = function (EnvValue) {
+      enviorment = EnvValue;
+    }
+
+    LightCallback = function (LightValue) {
+      light = LightValue;
+    }
+
+    Move.getMoveValue(MoveCallback)
+
+  }.bind(this), 500);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log("device enable");
+    res.render('index', { title: 'Express' });
+  
+  if(distance < 50){
+    res.redirect('/identify');
+  }
+});
+
+
+router.get('/identify', function(req, res, next) {
   res.render('index', { title: 'Express' });
+
 });
 
 module.exports = router;
---
+++ APD/routes/sensor.js
@@ -0,0 +1,88 @@
+const gpio = require('wiring-pi');
+const sleep = require('sleep');
+const temp = require('node-dht-sensor');
+const microt = require('microtime-nodejs');
+const McpAdc = require('mcp-adc');
+
+var sensor = require('./sensor.js');
+
+const DHT22 = 22;                    //wPi GPIO 7
+const ultraTRIG = 17;               //wPi GPIO 22
+const ultraECHO = 27;               //wPi GPIO 27
+
+var adc = new McpAdc.Mcp3208();
+var adcAudio = 0;                  //ADC Channel 0
+var adcEnv = 1;                  //ADC Channel 1
+var adcLight = 2;                  //ADC Channel 1
+
+
+gpio.wiringPiSetup();
+gpio.pinMode(ultraTRIG, gpio.OUTPUT);
+gpio.pinMode(ultraECHO, gpio.INPUT);
+
+module.exports.getTemp = function (callback) {
+    temp.read(22, DHT22, function (err, temperature, humidity) {
+        if (!err) {
+            console.log("temp :" + temperature.toFixed(1) + "C");
+            callback(temperature.toFixed(1));
+        }
+    });
+};
+
+module.exports.getHumi = function (callback) {
+    temp.read(22, DHT22, function (err, temperature, humidity) {
+        if (!err) {
+            console.log("humidity : " + humidity.toFixed(1) + "%");
+            callback(humidity.toFixed(1));
+        }
+    });
+};
+
+module.exports.getDist = function (callback) {
+    var distance = 0;
+    var pulse = 0;
+    while (1) {
+        gpio.digitalWrite(ultraTRIG, 0);
+        sleep.msleep(2);
+        gpio.digitalWrite(ultraTRIG, 1);
+        sleep.msleep(20);
+        gpio.digitalWrite(ultraTRIG, 0);
+
+        while (gpio.digitalRead(ultraECHO) == 0);
+        var startTime = microt.now();
+        while (gpio.digitalRead(ultraECHO) == 1);
+        var travelTime = microt.now();
+        distance = (travelTime - startTime) / 58;
+        output_dist = distance;
+        console.log("Distance:\t" + distance);
+
+         callback(distance);
+        sleep.msleep(300);
+    }
+
+};
+
+module.exports.getAdcAudio = function (callback) {
+    adc.readRawValue(adcAudio, function (value) {
+        console.log("Audio :\t" + value);
+        callback(value);
+    });
+
+};
+
+module.exports.getAdcEnv = function (callback) {
+    adc.readRawValue(adcEnv, function (value) {
+        console.log("Env:\t" + value);
+        callback(value);
+
+    });
+};
+
+module.exports.getAdcLight = function (callback) {
+
+    adc.readRawValue(adcLight, function (value) {
+        console.log("Light:\t" + value);
+        callback(value);
+
+    });
+};

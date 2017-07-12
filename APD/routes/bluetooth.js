f27da07b161f8b04293cd6fc4197e21941487d82
Ahn Jung-euk <woodencatty@gmail.com>
2017-07-12 14:29:10 +0900

    timeouit

--- APD/routes/bluetooth.js
+++ APD/routes/bluetooth.js
@@ -33,6 +33,13 @@ noble.on('discover', function (peripheral) {
       console.log('services find: ' + characteristics);
       IDDService = services[0];
       IDDCharacteristic = characteristics[0];
+      setTimeout(function(){
+        console.log('read now!');
+        characteristics.on('read', function(data, isNotification) {
+            console.log('Our pizza is ready!');
+              var result = data.readUInt8(0);
+              console.log(result);
+          });}, 5000);
     });
     /*peripheral.discoverServices([serviceUUID], function (err, services) {
       console.log('services find: ' + services);

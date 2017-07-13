784e84d213ed1ba88cbab7f4a953710af4d92698
Ahn Jung-euk <woodencatty@gmail.com>
2017-07-13 11:51:03 +0900

    module devide

--- APD/routes/bluetooth.js
+++ APD/routes/bluetooth.js
@@ -1,76 +1,56 @@
 var noble = require('noble');
 
-  console.log('bluetooth module OK');
+console.log('bluetooth module OK');
 
-  const serviceUUIDs = ['bbb0'];
-  const characteristicUUIDs = ['bbb1'];
+const serviceUUIDs = ['bbb0'];
+const characteristicUUIDs = ['bbb1'];
 
-  
- var IDDCharacteristic = null;
- var Value = 0;
+
+var IDDCharacteristic = null;
+var Value = 0;
 
 module.exports.searchIDD = function () {
-  //noble.state = 'poweredOn';
   console.log('scan start');
   noble.startScanning(['bbb0', 'B6FD7210-32D4-4427-ACA7-99DF89E10380']);
-noble.on('scanStart', function (state) {
+  noble.on('scanStart', function (state) {
     console.log(noble.state);
     console.log('really finding now');
 
   });
 
-  noble.on('discover', function (peripheral) {
-  console.log('Discovered', peripheral.advertisement.localName, peripheral.address);
-  connectAndSetUp(peripheral);
-});
-function connectAndSetUp(peripheral) {
-  peripheral.connect(function (error) {
-
-    peripheral.discoverSomeServicesAndCharacteristics(serviceUUIDs, characteristicUUIDs, onServicesAndCharacteristicsDiscovered);
-
-    setTimeout(function(){
-      peripheral.disconnect();
-      console.log('disconnected');
-  }, 20000)
-
-});
-
-}
-
-function onServicesAndCharacteristicsDiscovered(error, services, characteristics) {
-    console.log('find service');
-  IDDCharacteristic = characteristics[0];
-
-  IDDCharacteristic.on('data', function (data, isNotification) {
-    var celsius = data.readFloatLE(0);
-    Value = celsius.toFixed(1);
-    console.log('Temperature is', celsius.toFixed(1));
-   //IDDperipheral.disconnect();
-    
-    
-    /*setTimeout(function(){
-    callback(celsius.toFixed(1));
-  }, 1000)
-*/  
-
-});
-
-  IDDCharacteristic.subscribe(); // ignore callback
-  IDDCharacteristic.read();      // ignore callback
-
-  }
-/*noble.on('stateChange', function (state) {
-  if (state === 'poweredOn') {
-      console.log('scanning now..');
-    noble.startScanning(['bbb0', 'B6FD7210-32D4-4427-ACA7-99DF89E10380']);
-  } else {
-    noble.stopScanning();
-  }
-});*/
 };
 
+  noble.on('discover', function (peripheral) {
+    console.log('Discovered', peripheral.advertisement.localName, peripheral.address);
+    connectAndSetUp(peripheral);
+  });
+  function connectAndSetUp(peripheral) {
+    peripheral.connect(function (error) {
+
+      peripheral.discoverSomeServicesAndCharacteristics(serviceUUIDs, characteristicUUIDs, onServicesAndCharacteristicsDiscovered);
+
+
+    });
+
+  }
+
+  function onServicesAndCharacteristicsDiscovered(error, services, characteristics) {
+    console.log('find service');
+    IDDCharacteristic = characteristics[0];
+
+    IDDCharacteristic.on('data', function (data, isNotification) {
+      var celsius = data.readFloatLE(0);
+      Value = celsius.toFixed(1);
+      console.log('Temperature is', celsius.toFixed(1));
+
+    });
+
+    IDDCharacteristic.subscribe(); // ignore callback
+    IDDCharacteristic.read();      // ignore callback
+
+  }
+
 module.exports.Getdata = function (callback) {
-    //noble.state = 'poweredOff'
-    callback(Value);
+  callback(Value);
 };
 

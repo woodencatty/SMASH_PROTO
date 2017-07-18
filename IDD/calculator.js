3844c9b537e6e21524054f69bf5acba99dd9a762
Ahn Jung-euk <woodencatty@gmail.com>
2017-07-18 15:16:47 +0900

    this

--- IDD/calculator.js
+++ IDD/calculator.js
@@ -2,13 +2,6 @@ const Accel = require('./sensor.js');   //가속도 센서 모듈 import
 
 //센서의 X, Y, Z값을 받아온다.
 
-//센서값 콜백함수 정의
-AccelCallback = function (x, y, z) {
-    AccelX = Math.ceil(x*10);
-    AccelY = Math.ceil(y*10);
-    AccelZ = Math.ceil(z*10);
-}
-
 console.log('calculating..');
 //변환하여 저장할 값.
 
@@ -20,12 +13,20 @@ module.exports = {
 
 getMoveValue: function(){
 
+
+//센서값 콜백함수 정의
+AccelCallback = function (x, y, z) {
+    this.AccelX = Math.ceil(x*10);
+    this.AccelY = Math.ceil(y*10);
+    this.AccelZ = Math.ceil(z*10);
+}
+
     //가속도값 받아옴.
     Accel.getAccel(AccelCallback);
 
-    AccelX = AccelX*AccelX;
-    AccelY = AccelY*AccelY;
-    AccelZ = AccelZ*AccelZ;
+    this.AccelX = this.AccelX*this.AccelX;
+    this.AccelY = this.AccelY*this.AccelY;
+    this.AccelZ = this.AccelZ*this.AccelZ;
 
     MoveValue = Math.sqrt(AccelX + AccelY + AccelZ);
     console.log(MoveValue + '      x:'+AccelX+ '      y:'+AccelY+ '      z:'+AccelZ);

cf31e41d0d5e2cfd4d1311d432ce48a28ef02050
Ahn Jung-euk <woodencatty@gmail.com>
2017-07-19 16:34:34 +0900

    what test

---
+++ APD/routes/test.js
@@ -0,0 +1,16 @@
+var YourThing = require('YourThing');
+
+var id = 'P0001';
+YourThing.discoverById(function(yourThingInstance) {
+
+  // you can be notified of disconnects
+  yourThingInstance.on('disconnect', function() {
+    console.log('we got disconnected! :( ');
+  });
+
+  // you'll need to call connect and set up
+  yourThingInstance.connectAndSetUp(function(error) {
+    console.log('were connected!');
+  });
+
+});

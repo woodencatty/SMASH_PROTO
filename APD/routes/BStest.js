274d132e4bce1950b1072bf856da22be89ed8fef
Ahn Jung-euk <woodencatty@gmail.com>
2017-07-19 16:12:29 +0900

    work?

--- APD/routes/BStest.js
+++ APD/routes/BStest.js
@@ -1,5 +1,7 @@
 var btSerial = new (require('bluetooth-serial-port')).BluetoothSerialPort();
  
+ console.log('its running');
+ 
 btSerial.on('found', function(address, name) {
     btSerial.findSerialPortChannel(address, function(channel) {
         btSerial.connect(address, channel, function() { 
--- IDD/BStest.js
+++ IDD/BStest.js
@@ -1,5 +1,8 @@
 var server = new(require('bluetooth-serial-port')).BluetoothSerialPortServer();
  
+ console.log('its running');
+
+
 var CHANNEL = 10; // My service channel. Defaults to 1 if omitted. 
 var UUID = '38e851bc-7144-44b4-9cd8-80549c6f2912'; // My own service UUID. Defaults to '1101' if omitted 
  

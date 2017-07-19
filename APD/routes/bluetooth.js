a5b6b664cb540879fab04e3f1704537fbbdc8f01
Ahn Jung-euk <woodencatty@gmail.com>
2017-07-19 15:43:47 +0900

    step1 identify chara..

--- APD/routes/bluetooth.js
+++ APD/routes/bluetooth.js
@@ -38,9 +38,11 @@ module.exports = {
 function connectAndSetUp(peripheral) {
       peripheral.connect( (error)=> {
        peripheral.discoverSomeServicesAndCharacteristics(serviceUUIDs, characteristicUUIDs, (error, services, characteristics)=>{
-         console.log(services);
-         console.log(characteristics);
-         
+        console.log( characteristics[0]);
+         characteristics[0].read((error, data)=>{
+              console.log(data);
+              console.log(error);
+         })
        });
       });
 

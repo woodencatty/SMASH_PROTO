f357702c75a3016d9e4f2bb82cb826cf2c032d29
Ahn Jung-euk <woodencatty@gmail.com>
2017-07-19 17:29:15 +0900

    set to ble.js

--- IDD/bluetooth.js
+++ IDD/bluetooth.js
@@ -1,23 +1,17 @@
-const bleno = require('bleno');           //Bluetooth 수신부(peripheral) 모듈
-const util = require('util');             //유틸리티 모듈. inherits함수를 사용하기 위함.
+var bleno = require('bleno');
+var util = require('util');
 
-
-//bluetooth 구성 변수들 정의
 var Characteristic = bleno.Characteristic;
 var Descriptor = bleno.Descriptor;
 var PrimaryService = bleno.PrimaryService;
 
-//센서값, ID값 초기화
-var IDDvalue;
-var IDDID;
+var temperatureSensorId;
+var lastTemp;
 
-//주기적으로 운동량값을 저장함(임시)
-
-//bluetooth Characteristic 정의
 var IDDCharacteristic = function () {
   IDDCharacteristic.super_.call(this, {
     uuid: 'bbb1',
-    properties: ['read', 'notify'],
+    properties: ['read'],
     descriptors: [
       new Descriptor({
         uuid: '2901',
@@ -27,7 +21,14 @@ var IDDCharacteristic = function () {
 };
 util.inherits(IDDCharacteristic, Characteristic);
 
-//Bluetooth Service 정의
+IDDCharacteristic.prototype.onReadRequest = function (offset, callback) {
+  var result = 10.10;
+    console.log('Sensor ' + temperatureSensorId + ' :', result);
+    var data = new Buffer(4);
+    data.writeFloatLE(result, 0);
+    callback(Characteristic.RESULT_SUCCESS, data);
+};
+
 var IDDService = new PrimaryService({
   uuid: 'bbb0',
   characteristics: [
@@ -35,73 +36,29 @@ var IDDService = new PrimaryService({
   ]
 });
 
-//Bluetooth Central모듈에서 Subscribe 요청을 보냈을 때의 경우
-IDDCharacteristic.prototype.onSubscribe = function (maxValueSize, updateValueCallback) {
-  console.log('IDDCharacteristic subscribe');
+bleno.on('stateChange', function (state) {
+  console.log('on -> stateChange: ' + state);
 
-  //주기적으로 데이터를 전송함.
-  this.changeInterval = setInterval(function () {
-    var data = new Buffer(4);
-    data.writeFloatLE(IDDvalue, 0);
-
-    console.log('sending : ' + IDDvalue);
-    updateValueCallback(data);
-
-  }.bind(this), 500);
-};
-
-//Bluetooth Central모듈에서 unSubscribe 요청을 보냈을 때의 경우
-IDDCharacteristic.prototype.onUnsubscribe = function () {
-  console.log('IDDCharacteristic unsubscribe');
-
-  //데이터 전송 주기를 해제함.
-  if (this.changeInterval) {
-    clearInterval(this.changeInterval);
-    this.changeInterval = null;
+  if (state === 'poweredOn') {
+    bleno.startAdvertising('P0001', [IDDService.uuid]);
+  } else {
+    bleno.stopAdvertising();
   }
-};
-
-//Bluetooth Central모듈에서 Read 요청을 보냈을 때의 경우
-IDDCharacteristic.prototype.onReadRequest = function (offset, callback) {
-  //데이터 전송
-  var data = new Buffer(4);
-  data.writeFloatLE(IDDvalue, 0);
-  callback(Characteristic.RESULT_SUCCESS, data);
-};
-
+});
 
 bleno.on('advertisingStart', function (error) {
   console.log('on -> advertisingStart: ' + (error ? 'error ' + error : 'success'));
+
   if (!error) {
     bleno.setServices([IDDService]);
   }
 });
 
-bleno.on('disconnect', (clientAddress)=>{
-  console.log(clientAddress + ' is disconnected.');
-});
-
-
 bleno.on('accept', (clientAddress)=>{
-  console.log(clientAddress + ' is connected.');
+    console.log('conntected to '+ clientAddress);
 });
 
+bleno.on('disconnect', (clientAddress)=>{
+        console.log('disconnect to '+ clientAddress);
 
-//Bluetooth 탐색 기능 실행 모듈화.
-module.exports.AdvertisingDevice = function (ID) {
-
-  IDDID = ID;
-  bleno.on('stateChange', function (state) {
-    console.log('on -> stateChange: ' + state);
-    if (state === 'poweredOn') {
-      bleno.startAdvertising(IDDID, [IDDService.uuid]);
-    } else {
-      bleno.stopAdvertising();
-    }
-  });
-};
-
-module.exports.SetStepValue = function (value) {
-  IDDvalue = value;
-  console.log('value setted : ' + IDDvalue);
-};
+});

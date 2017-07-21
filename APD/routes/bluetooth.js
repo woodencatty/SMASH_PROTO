0aea5a96a8fb947261819a1e143dff462b79ba1a
Ahn Jung-euk <woodencatty@gmail.com>
2017-07-21 10:53:03 +0900

    add log

--- APD/routes/bluetooth.js
+++ APD/routes/bluetooth.js
@@ -48,19 +48,11 @@ function onServicesAndCharacteristicsDiscovered(error, services, characteristics
 }
 
 
-noble.on('stateChange', function (state) {
-  if (state === 'poweredOn') {
-    this.try_count++;
-    noble.startScanning(['bbb0', 'B6FD7210-32D4-4427-ACA7-99DF89E10380']);
-  } else {
-    noble.stopScanning();
-  }
-});
-
 module.exports = {
   startSearch: () => {
     noble.on('stateChange', function (state) {
       if (state === 'poweredOn') {
+        console.log('scanning');
         this.try_count++;
         noble.startScanning(['bbb0', 'B6FD7210-32D4-4427-ACA7-99DF89E10380']);
       } else {

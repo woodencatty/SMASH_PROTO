c6f0f4e89e7a4c2735574b96e058c7a5d8a03ab9
Ahn Jung-euk <woodencatty@gmail.com>
2017-07-27 21:38:29 +0900

    fix

--- APD/routes/index.js
+++ APD/routes/index.js
@@ -241,7 +241,7 @@ router.get('/return2main', (req, res, next) => {
 });
 
 //업데이트 파일 전송시 수행하는 모듈.
-app.use(express.bodyParser());
+router.use(express.bodyParser());
 router.post('/SWserver/metadata/APDUpdate', (req, res, next) => {
 
 

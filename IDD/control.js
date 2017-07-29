bb550f76cf0ef11be834c3eaf12826e160f452a1
Ahn Jung-euk <woodencatty@gmail.com>
2017-07-29 12:54:05 +0900

    fix

--- IDD/control.js
+++ IDD/control.js
@@ -40,9 +40,9 @@ this.statusInterval = setInterval(() => {
 
 initialize();
 function loggingInterval(loggingInterval, filename,fsOption) {
-
   //5초에 한번 걸음 수를 업데이트하여 로그에 저장함.
   this.loggingInterval = setInterval(() => {
+        acturator.led_dataSaved();
     WalkCallback = function (WalkCount) {
       fs.open(filename, fsOption, function (err, fd) {
         if (err) throw err;
@@ -58,7 +58,6 @@ function loggingInterval(loggingInterval, filename,fsOption) {
       //bluetooth.SetStepValue(WalkCount);
     }
     move.getWalkCount(WalkCallback);
-    acturator.led_dataSaved();
   }, loggingInterval);
 }
 
